d380a2000a09568e729bcbb9b0ec7ee2ef8dd71c
laibakiyani840 <laibakiyani840@gmail.com>
2025-10-23 20:53:16 -0700

    Introduction_to_Python

---
+++ main.py
@@ -0,0 +1,8 @@
+# main.py
+import myMath
+result = myMath.add(5, 3)
+print(result)
+result1 = myMath.subtract(10, 4)
+print(result1)
+result2 = myMath.multiply(6, 7)
+print(result2)
---
+++ myMath.py
@@ -0,0 +1,10 @@
+# mymath.py
+def add(x, y):
+    return x + y
+def subtract(x, y):
+    return x - y
+pi= 3.14159
+def multiply(x, y):
+    return x * y    
+
+
